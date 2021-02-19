1e3dffc4c8c67b3b422e2b56a1da29884bcdbc6f
grigoriiev <39682102+grigoriiev@users.noreply.github.com>
2021-02-19 22:45:57 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/author/list',[AuthorController::class,'list']);
+
+Route::post('/author/add',[AuthorController::class,'add']);
+
+Route::post('/author/update',[AuthorController::class,'update']);
+
+Route::post('/author/delete',[AuthorController::class,'delete']);
+
+Route::get('/magazine/list',[MagazineController::class,'list']);
+
+Route::post(' /magazine/add',[MagazineController::class,'add']);
+
+Route::post(' /magazine/update',[MagazineController::class,'update']);
+
+Route::post(' /magazine/delete',[MagazineController::class,'delete']);
